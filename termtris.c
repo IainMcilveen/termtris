cee6b95f23788f98a6445fa0b970d5cf54cf0558
IainMcilveen <Bugatti.iain@gmail.com>
2022-01-09 20:25:40 -0400

    add ncurses

--- termtris.c
+++ termtris.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <ncurses.h>
 #include "termtris.h"
 
 int main() {
-    printf("This is a test: %s\n",test);
+
+    initscr();              // start ncurses
+    printw("Hello World");  // print hello world
+    refresh();              // print it onto the real screen
+    getch();                // wait for user input
+    endwin();               // end ncurses
+    
+    // game loop
+    while(gameRunning) {
+        updateInput();
+        update();
+        render();
+        gameRunning = 0;
+    }
+
+    return 0;
+}
+
+void updateInput() {
+    printf("Inputs \n");
+}
+
+void update() {
+    printf("update \n");
+}
+
+void render() {
+    printf("Render \n");
 }
--- termtris.h
+++ termtris.h
@@ -1,1 +1,9 @@
-#define test    "test define"
+//#define test    "test define"
+
+// Define Variables
+int gameRunning = 1;
+
+// Define Function
+void updateInput();
+void update();
+void render();
