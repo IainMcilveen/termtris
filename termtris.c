3383e2daee497b19d07ed420f45900bb341edb99
IainMcilveen <Bugatti.iain@gmail.com>
2022-01-09 21:25:49 -0400

    get inputs, still broken

--- termtris.c
+++ termtris.c
@@ -1,34 +1,72 @@
-#include <stdio.h>
-#include <ncurses.h>
 #include "termtris.h"
 
 int main() {
 
-    initscr();              // start ncurses
-    printw("Hello World");  // print hello world
-    refresh();              // print it onto the real screen
-    getch();                // wait for user input
-    endwin();               // end ncurses
+    // initialize
+    init();
     
     // game loop
     while(gameRunning) {
-        updateInput();
+        gameRunning = updateInput();
         update();
         render();
-        gameRunning = 0;
     }
 
+    endwin();               // end ncurses
     return 0;
 }
 
-void updateInput() {
-    printf("Inputs \n");
+// initalize everything
+void init() {
+    
+    // init ncurses
+    initscr();              // start ncurses
+    cbreak();               // disable line buffering
+    noecho();               // don't show input on screen
+    nodelay(stdscr, TRUE);  // no delay on inputs
+
+    // init board
+    for(int y = 0; y < HEIGHT; y++) {
+        for(int x = 0; x < WIDTH; x++) {
+            board[y][x] = ' ';
+        }
+    }
+
+    // temp
+    board[0][0] = 'x';
+    board[19][9] = 'x';
+    board[19][0] = 'x';
+    board[0][9] = 'x';
+
+}
+
+int updateInput() {
+
+    // get user inputs
+    char c;
+    while((c = getch()) != ERR){
+        if(c == 'q'){
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 void update() {
-    printf("update \n");
+
 }
 
 void render() {
-    printf("Render \n");
+
+    // draw board
+    for(int y = 0; y < HEIGHT; y++) {
+        for(int x = 0; x < WIDTH; x++) {
+            mvaddch(y,x,board[y][x]);
+        }
+    }
+
+    refresh();              // print it onto the real screen
+
+    getch();
 }
--- termtris.h
+++ termtris.h
@@ -1,9 +1,17 @@
-//#define test    "test define"
+// includes
+#include <ncurses.h> // includes stdio.h
+
+// define constants
+#define WIDTH   10
+#define HEIGHT  20
 
 // Define Variables
 int gameRunning = 1;
+char board[HEIGHT][WIDTH];
+char input[] = {' ',' ',' ',' ',' ',' '};   //{'A','D','N','M','Q','S'} S will be unused for now
 
 // Define Function
-void updateInput();
+void init();
+int updateInput();
 void update();
 void render();
